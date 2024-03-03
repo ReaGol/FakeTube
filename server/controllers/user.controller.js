b4510ecdf0acf78d6b10576b704c1ff402d69a3b
ReaGol <reutgoldin@gmail.com>
2024-03-03 13:05:04 +0200

    added user CRUD operations

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -29,8 +29,53 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-export const getUser = async (req, res, next) => {};
-export const subscribe = async (req, res, next) => {};
-export const unsubscribe = async (req, res, next) => {};
-export const like = async (req, res, next) => {};
-export const dislike = async (req, res, next) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("Subscription successfull")
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const unsubscribe = async (req, res, next) => {
+  try {
+        await User.findById(req.user.id, {
+          $pull: { subscribedUsers: req.params.id },
+        });
+        await User.findByIdAndUpdate(req.params.id, {
+          $inc: { subscribers: -1 },
+        });
+        res.status(200).json("Unsubscribed successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
