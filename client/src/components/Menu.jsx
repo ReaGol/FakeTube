bfbc1fb54df79fe63bdf90cc4860251a0c2eb55d
ReaGol <reutgoldin@gmail.com>
2024-03-05 11:05:09 +0200

    added favicon, added menu icons

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,40 +1,124 @@
-import React from 'react'
-import styled from 'styled-components'
-import FakeTube from '../imgs/logo.png'
+import React from "react";
+import styled from "styled-components";
+import FakeTube from "../imgs/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import MovieIcon from "@mui/icons-material/Movie";
+import ArticleIcon from "@mui/icons-material/Article";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SettingsIcon from "@mui/icons-material/Settings";
+import FlagIcon from "@mui/icons-material/Flag";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
 
 const Container = styled.div`
-flex:1;
-background-color: #202020;
-height:100vh;
-color:white;
-font-size:14px;
-`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+  font-size: 14px;
+`;
 const Wrapper = styled.div`
-padding: 18px 26px;
-`
+  padding: 18px 26px;
+`;
 
 const Logo = styled.div`
-display:flex;
-align-items: center;
-gap: 5px;
-font-weight:bold;
-margin-bottom:25px;
-`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
 const Img = styled.img`
-height:25px;
-`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 
 function Menu() {
   return (
     <Container>
-        <Wrapper>
-            <Logo>
-                <Img src={FakeTube}/>
-                FakeTube
-            </Logo>
-        </Wrapper>
+      <Wrapper>
+        <Logo>
+          <Img src={FakeTube} />
+          FakeTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsIcon />
+          Subscriptions
+        </Item>
+        <hr />
+        <Item>
+          <VideoLibraryIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryIcon />
+          History
+        </Item>
+        <hr />
+        <Item>
+          <LibraryMusicIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvIcon />
+          Live
+        </Item>
+        <hr />
+        <Item>
+          <SettingsIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineIcon />
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessIcon />
+          Light Mode
+        </Item>
+      </Wrapper>
     </Container>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
